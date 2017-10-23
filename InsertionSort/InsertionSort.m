70f1a91b7f084f01d7a8f9bf2f96b894a0b8f97c
Yongjai <dev.yongjai@gmail.com>
2017-10-23 17:01:23 +0900

    Update README.md

--- InsertionSort/InsertionSort.m
+++ InsertionSort/InsertionSort.m
@@ -10,6 +10,12 @@
 
 @implementation InsertionSort
 
+
+/* Insertion Sort
+ for example, there is a array like {4, 3, 1, 2}.
+ Convert steps like {3, 4, 1, 2} -> {3, 1, 4, 2} => {1, 3, 4, 2} -> {1, 3, 2, 4} -> {1, 2, 3, 4}
+ So, Result is {1, 2, 3, 4}
+ */
 + (NSMutableArray *)InsertionSorts: (NSMutableArray*)array {
     for (int i = 1; i < array.count; i++) {
         for (int j = i; (j > 0) && ([array[j-1] intValue] > [array[j] intValue]); j--) {
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ### Objective-C로 알고리즘을 구현해서 정리하는 Repository 입니다.
 
-* Insertion Sort
+
+* [Insertion Sort](https://github.com/Yongjai/Algorithm_Objective-C/blob/master/InsertionSort/InsertionSort.m)
